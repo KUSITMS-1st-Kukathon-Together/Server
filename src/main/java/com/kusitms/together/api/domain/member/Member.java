f286924579d66ec03f5d5b8a75be1b93bcf2c9a3
mingeun0507 <tqdlfkg@naver.com>
2023-03-25 16:20:08 +0900

    feat: Member Entity 구현

---
+++ src/main/java/com/kusitms/together/api/domain/member/Member.java
@@ -0,0 +1,44 @@
+package com.kusitms.together.api.domain.member;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "Memeber")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "login_id")
+    private String loginId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "profile_img")
+    private String profileImage;
+
+    @Builder
+    public Member(String loginId, String password, String nickname, String location, String profileImage) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+        this.location = location;
+        this.profileImage = profileImage;
+    }
+}
