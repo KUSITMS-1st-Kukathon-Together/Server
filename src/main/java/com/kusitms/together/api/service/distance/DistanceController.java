9e80149ce86915228e1854b7da2bf0afabc18c2b
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-03-26 00:33:58 +0900

    feat:/main method get으로 변경

--- src/main/java/com/kusitms/together/api/service/distance/DistanceController.java
+++ src/main/java/com/kusitms/together/api/service/distance/DistanceController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,24 +17,13 @@ public class DistanceController {
 
     private final DistanceService distanceService;
 
-    @ResponseBody
-    @PostMapping("/near")
-    public ResponseEntity<List<DistanceDto>> getNearPosts(@RequestBody DistanceReq req) {
-        return new ResponseEntity<>(distanceService.getNearPosts(req.getLatitude(), req.getLongitude()), HttpStatus.OK);
-    }
-
-    @ResponseBody
-    @PostMapping("/main")
-    public ResponseEntity<PageRes> mainPage(@RequestBody MainReq mainReq) {
+    @GetMapping("/main")
+    public ResponseEntity<PageRes> mainPage(@ModelAttribute MainReq mainReq) {
 
         List<DistanceDto> nearPosts = distanceService.getNearPosts(mainReq.getDistanceReq().getLatitude(), mainReq.getDistanceReq().getLongitude());
         List<DistanceDto> allPost = distanceService.getAllPost(Category.valueOf(mainReq.getCategoryAndTypeReq().getCategory()), Type.valueOf(mainReq.getCategoryAndTypeReq().getType()));
 
         return new ResponseEntity<>(new PageRes(nearPosts,allPost), HttpStatus.OK);
     }
-    @ResponseBody
-    @PostMapping("/all")
-    public ResponseEntity<List<DistanceDto>> getAllPosts(@RequestBody CategoryAndTypeReq CategoryAndTypeReq) {
-        return new ResponseEntity<>(distanceService.getAllPost(Category.valueOf(CategoryAndTypeReq.getCategory()), Type.valueOf(CategoryAndTypeReq.getType())), HttpStatus.OK);
-    }
+
 }
