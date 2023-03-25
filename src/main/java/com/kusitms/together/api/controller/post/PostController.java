2d83e9842c6306804ace6b8be49d231c562db02a
mingeun0507 <tqdlfkg@naver.com>
2023-03-26 02:05:04 +0900

    feat: 댓글 작성 API 추가

--- src/main/java/com/kusitms/together/api/controller/post/PostController.java
+++ src/main/java/com/kusitms/together/api/controller/post/PostController.java
@@ -1,8 +1,11 @@
 package com.kusitms.together.api.controller.post;
 
 import com.kusitms.together.api.domain.member.MemberAccount;
+import com.kusitms.together.api.dto.comment.request.WriteCommentRequestDto;
+import com.kusitms.together.api.dto.comment.response.CommentResponseDto;
 import com.kusitms.together.api.dto.post.request.WritePostRequestDto;
 import com.kusitms.together.api.dto.post.response.PostResponseDto;
+import com.kusitms.together.api.service.comment.CommentService;
 import com.kusitms.together.api.service.distance.DistanceDto;
 import com.kusitms.together.api.service.post.PostService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +20,8 @@ public class PostController {
 
     private final PostService postService;
 
+    private final CommentService commentService;
+
     @PostMapping
     public ResponseEntity<PostResponseDto> writePost(@AuthenticationPrincipal MemberAccount memberAccount, @RequestBody WritePostRequestDto writePostRequestDto) {
         return ResponseEntity.ok(postService.writePost(memberAccount.getMemberId(), writePostRequestDto));
@@ -25,5 +30,10 @@ public class PostController {
     @GetMapping("/{postId}")
     public ResponseEntity<PostResponseDto> getPost(@PathVariable Long postId) {
         return ResponseEntity.ok(postService.getPost(postId));
+    }
+
+    @PostMapping("/{postId}/comment")
+    public ResponseEntity<CommentResponseDto> writeComment(@AuthenticationPrincipal MemberAccount memberAccount, @PathVariable Long postId, @RequestBody WriteCommentRequestDto writeCommentRequestDto) {
+        return ResponseEntity.ok(commentService.createComment(memberAccount.getMemberId(), postId, writeCommentRequestDto));
     }
 }
