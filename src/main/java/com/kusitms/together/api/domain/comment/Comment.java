eabbcba8012a09edc103424a194ea9592a09c0df
mingeun0507 <tqdlfkg@naver.com>
2023-03-25 17:19:48 +0900

    feat: 댓글 Entity 구현

---
+++ src/main/java/com/kusitms/together/api/domain/comment/Comment.java
@@ -0,0 +1,42 @@
+package com.kusitms.together.api.domain.comment;
+
+import com.kusitms.together.api.domain.member.Member;
+import com.kusitms.together.api.domain.post.Post;
+import com.kusitms.together.api.domain.util.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
+
+@Getter
+
+@NoArgsConstructor
+@Entity
+@Table(name = "OG_COMMENT")
+public class Comment extends BaseTimeEntity {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "comment_id")
+        private Long id;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "post_id", nullable = false)
+        private Post post;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "member_id", nullable = false)
+        private Member member;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "comment_id", nullable = false)
+        private Comment parentComment;
+
+        @Column(name = "content")
+        private String content;
+
+        @Column(name = "is_child")
+        private Boolean isChild;
+
+}
