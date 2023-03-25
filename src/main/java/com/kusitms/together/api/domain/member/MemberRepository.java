b7540787f32a8c8c086510f5c3af25a8f21f6c74
mingeun0507 <tqdlfkg@naver.com>
2023-03-25 22:00:01 +0900

    feat: 로그인을 위한 메소드 구현

--- src/main/java/com/kusitms/together/api/domain/member/MemberRepository.java
+++ src/main/java/com/kusitms/together/api/domain/member/MemberRepository.java
@@ -2,5 +2,8 @@ package com.kusitms.together.api.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>{
+    Optional<Member> findByLoginId(String loginId);
 }
