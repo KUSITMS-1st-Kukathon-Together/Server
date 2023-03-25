9b392fd3f93cf0dc1b232bf1d614603f9247c0c8
mingeun0507 <tqdlfkg@naver.com>
2023-03-25 16:34:26 +0900

    feat: 게시글 엔티티 구현

---
+++ src/main/java/com/kusitms/together/api/domain/post/Post.java
@@ -0,0 +1,58 @@
+package com.kusitms.together.api.domain.post;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "Post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long idd;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "category")
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Column(name = "address")
+    private String address;
+
+    @Builder
+    public Post(String location, String title, String content, Category category, Type type, Double latitude, Double longitude, String address) {
+        this.location = location;
+        this.title = title;
+        this.content = content;
+        this.category = category;
+        this.type = type;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
+    }
+}
