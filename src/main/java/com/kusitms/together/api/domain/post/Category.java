c38b9e387a5a3d56464681079d187741de4128a6
mingeun0507 <tqdlfkg@naver.com>
2023-03-26 00:27:21 +0900

    feat: enum 열거체 jsoncreator 추가

--- src/main/java/com/kusitms/together/api/domain/post/Category.java
+++ src/main/java/com/kusitms/together/api/domain/post/Category.java
@@ -1,5 +1,9 @@
 package com.kusitms.together.api.domain.post;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Data;
+
 public enum Category {
     공사_및_점검, // 공사 및 점검
     사고_상황, // 사고 상황
@@ -9,4 +13,15 @@ public enum Category {
     취미_생활, // 취미 생활
     일상, // 일상
     기타; // 기타
+
+    @JsonCreator
+    public static Category of(String category) {
+        for (Category c : Category.values()) {
+            if (c.name().equals(category)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
 }
