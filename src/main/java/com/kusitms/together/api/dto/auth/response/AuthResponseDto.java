461f518aefc8aea7b816dff644d6fe3a70ced80d
mingeun0507 <tqdlfkg@naver.com>
2023-03-25 22:00:23 +0900

    feat: 로그인 DTO 구현 및 응답 DTO 수정

--- src/main/java/com/kusitms/together/api/dto/auth/response/AuthResponseDto.java
+++ src/main/java/com/kusitms/together/api/dto/auth/response/AuthResponseDto.java
@@ -1,17 +1,16 @@
 package com.kusitms.together.api.dto.auth.response;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class SignUpResponseDto {
+public class AuthResponseDto {
     private String message;
     private String token;
 
-    public SignUpResponseDto(String message, String token) {
+    public AuthResponseDto(String message, String token) {
         this.message = message;
         this.token = token;
     }
