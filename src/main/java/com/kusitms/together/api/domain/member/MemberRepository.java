5a0f4e41c68ea523239c08f9ce674458d75932e1
mingeun0507 <tqdlfkg@naver.com>
2023-03-26 04:31:30 +0900

    feat: ID 중복 처리를 위한 메소드 구현

--- src/main/java/com/kusitms/together/api/domain/member/MemberRepository.java
+++ src/main/java/com/kusitms/together/api/domain/member/MemberRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long>{
     Optional<Member> findByLoginId(String loginId);
+
+    Boolean existsByLoginId(String loginId);
 }
