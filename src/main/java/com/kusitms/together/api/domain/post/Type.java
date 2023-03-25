86b8b0b83766e46d65aeb8b6470f82c9aab9cd29
mingeun0507 <tqdlfkg@naver.com>
2023-03-26 00:27:21 +0900

    feat: enum 열거체 jsoncreator 추가

--- src/main/java/com/kusitms/together/api/domain/post/Type.java
+++ src/main/java/com/kusitms/together/api/domain/post/Type.java
@@ -1,5 +1,17 @@
 package com.kusitms.together.api.domain.post;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Type {
     WATCH_OUT, RECOMMEND, ETC;
+
+    @JsonCreator
+    public static Type of(String type) {
+        for (Type t : Type.values()) {
+            if (t.name().equals(type)) {
+                return t;
+            }
+        }
+        return null;
+    }
 }
